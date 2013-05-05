16bc6f9825f71cd8e346420e59bbc36274a69af8
Otto Allmendinger <otto.allmendinger@gmail.com>
2013-05-05 19:52:01 +0200

    initial preferences dialog

--- src/prefs.js
+++ src/prefs.js
@@ -1,3 +1,5 @@
+const Lang = imports.lang;
+
 const Gtk = imports.gi.Gtk;
 const Gio = imports.gi.Gio;
 const GObject = imports.gi.GObject;
@@ -8,22 +10,11 @@ const N_ = function(e) { return e; };
 
 const Extension = imports.misc.extensionUtils.getCurrentExtension();
 const Convenience = Extension.imports.convenience;
-
 const ApiProvider = Extension.imports.ApiProvider;
 
-const SETTINGS_SCHEMA = 'org.gnome.shell.extensions.bitcoin-markets';
-const SETTINGS_FIRST_TIME = 'first-time';
-const SETTINGS_MARKET = 'market';
-const SETTINGS_CURRENCY = 'currency';
+const INDICATORS_KEY = "indicators";
 
-
-let _settings;
-
-let initSettings = function () {
-    _settings = Extension.getSettings();
-}
-
-let _defaults = [
+const _defaults = [
     {
         api: 'mtgox',
         currency: 'USD',
@@ -35,30 +26,164 @@ let _defaults = [
     }
 ];
 
-const BitcoinMarketsSettingsWidget = new GObject.Class({
-    Name: "BitcoinMarkets.BitcoinMarketsSettingsWidget",
-    GTypeName: "BitcoinMarketsSettingsWidget",
-    Extends: Gtk.Grid,
+const IndicatorCollectionModel = new GObject.Class({
+    Name: "BitcoinMarkets.IndicatorListModel",
+    GTypeName: "IndicatorListModel",
+    Extends: Gtk.ListStore,
+
+    Columns: {
+      LABEL: 0,
+      CONFIG: 1
+    },
 
     _init: function (params) {
         this.parent(params);
+        this.set_column_types([GObject.TYPE_STRING, GObject.TYPE_STRING]);
 
-        this.margin = 10;
-        this.orientation = Gtk.Orientation.VERTICAL;
+        this._settings = Convenience.getSettings();
 
-        this.add(new Gtk.Label({label: _("API Provider")}));
+        this._reloadFromSettings();
 
-        for (let i=0; i < 10; i++) {
-            this.attach(
-                new Gtk.Label({label: _("foo " + i)}),
-                i, 2, 1, 1);
+        this.connect('row-changed', Lang.bind(this, this._onRowChanged));
+        this.connect('row-inserted', Lang.bind(this, this._onRowInserted));
+        this.connect('row-deleted', Lang.bind(this, this._onRowDeleted));
+    },
 
+    _getLabel: function (config) {
+        return config.api + " " + config.currency;
+    },
+
+    _reloadFromSettings: function () {
+        this.clear();
+
+        let configs = this._settings.get_strv(INDICATORS_KEY);
+
+        for each (let json in configs) {
+            let c = JSON.parse(json);
+            this.set(this.append(),
+                    [this.Columns.LABEL, this.Columns.CONFIG],
+                    [this._getLabel(c), json]);
         }
-    }
+    },
+
+    _onRowChanged: function () true,
+    _onRowInserted: function () true,
+    _onRowDeleted: function () true
+});
+
+const BitcoinMarketsSettingsWidget = new GObject.Class({
+    Name: "BitcoinMarkets.BitcoinMarketsSettingsWidget",
+    GTypeName: "BitcoinMarketsSettingsWidget",
+    Extends: Gtk.Box,
+
+    _init: function (params) {
+        this.parent(params);
+        this.margin = 10;
+        this.orientation = Gtk.Orientation.HORIZONTAL;
+
+        this._store = new IndicatorCollectionModel();
+
+        this._initLayout();
+        this._initBehavior();
+
+        // this.add(new Gtk.Label({label: _("API Provider")}));
+    },
+
+    _initLayout: function () {
+        this._indicatorList = new Gtk.Box({
+            orientation: Gtk.Orientation.VERTICAL,
+            'width-request': 200
+        });
+
+        this._treeView = new Gtk.TreeView({
+            model: this._store,
+            headers_visible: true,
+            reorderable: true,
+            hexpand: false,
+            vexpand: true
+        });
+
+        let label = new Gtk.TreeViewColumn({title: "Label"});
+        let renderer = new Gtk.CellRendererText()
+        label.pack_start(renderer, true);
+        label.add_attribute(renderer, "text", 0);
+        this._treeView.insert_column(label, 0);
+
+        this._indicatorList.add(this._treeView);
+
+        let toolbar = new Gtk.Toolbar();
+        toolbar.get_style_context().add_class(Gtk.STYLE_CLASS_INLINE_TOOLBAR);
+
+        let newButton = new Gtk.ToolButton({stock_id: Gtk.STOCK_NEW});
+        newButton.connect('clicked', Lang.bind(this, this._newClicked));
+        toolbar.add(newButton);
+
+        let delButton = new Gtk.ToolButton({stock_id: Gtk.STOCK_DELETE});
+        delButton.connect('clicked', Lang.bind(this, this._delClicked));
+        toolbar.add(delButton);
+
+        this._indicatorList.add(toolbar);
+
+        this.add(this._indicatorList);
+
+
+
+
+        this._configGrid = new Gtk.Grid({
+            orientation: Gtk.Orientation.VERTICAL,
+            margin: 10
+        });
+
+
+        this.add(this._configGrid);
+    },
+
+    _initBehavior: function () {
+        this._selection = this._treeView.get_selection();
+        this._selection.connect(
+                'changed',
+                Lang.bind(this, this._onSelectionChanged)
+        );
+    },
+
+    _onSelectionChanged: function () {
+        let [isSelected, model, iter] = this._selection.get_selected();
+        let json = this._store.get_value(
+            iter, this._store.Columns.CONFIG
+        );
+        this._displayConfig(JSON.parse(json));
+    },
+
+    _displayConfig: function (config) {
+
+        for each (let w in this._configGridWidgets) {
+            this._configGrid.remove(w);
+        }
+
+        this._configGridWidgets = [];
+
+        let [x, y] = [1, 1];
+
+        let add = Lang.bind(this, function (w) {
+            this._configGridWidgets.push(w);
+            this._configGrid.attach(w, x, y, 1, 1);
+        });
+
+        add(new Gtk.Label({label: _("Api Provider")}));
+        y += 1;
+
+        add(new Gtk.Label({label: _("Currency")}));
+        y += 1;
+
+        this._configGrid.show_all();
+    },
+
+    _newClicked: function () true,
+
+    _delClicked: function () true
 });
 
 function init() {
-    initSettings();
     Convenience.initTranslations();
 }
 
