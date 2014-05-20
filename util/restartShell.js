ffeaab1d765fbc5fe31673bfc4541a0923dfc83d
Otto Allmendinger <otto.allmendinger@gmail.com>
2014-05-20 18:20:09 +0200

    bye bye xml literals?

--- util/restartShell.js
+++ util/restartShell.js
@@ -1,12 +1,14 @@
 const Gio = imports.gi.Gio;
 
 const MyIface =
-    <interface name="org.gnome.Shell">
-        <method name="Eval">
-            <arg type="s" direction="in" />
-            <arg type="bs" direction="out" />
-        </method>
-    </interface>;
+    '<node>\
+    <interface name="org.gnome.Shell">\
+        <method name="Eval">\
+            <arg type="s" direction="in" />\
+            <arg type="bs" direction="out" />\
+        </method>\
+    </interface>\
+    </node>';
 
 const MyProxy = Gio.DBusProxy.makeProxyWrapper(MyIface);
 
