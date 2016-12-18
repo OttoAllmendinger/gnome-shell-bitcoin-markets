c94a5cb46f89a6b7c915ee864d9fe12496b940d0
Otto Allmendinger <otto.allmendinger@gmail.com>
2016-12-18 13:36:29 +0100

    split HTTP

--- src/BaseProvider.js
+++ src/BaseProvider.js
@@ -5,89 +5,16 @@ const Signals = imports.signals;
 const Mainloop = imports.mainloop;
 
 const Local = imports.misc.extensionUtils.getCurrentExtension();
+const HTTP = Local.imports.HTTP;
 const Accounting = Local.imports.accounting.accounting;
 const { CurrencyData } = Local.imports.CurrencyData;
 const { IndicatorModel } = Local.imports.IndicatorModel;
-
-const HTTP_TOO_MANY_REQUESTS = 429;
-
-/*
- * Init HTTP
- *
- */
-
-const getExtensionVersion = function () {
-  if (Local.metadata['git-version']) {
-    return 'git-' + Local.metadata['git-version'];
-  } else if (Local.metadata.version) {
-    return 'v' + Local.metadata.version;
-  } else {
-    return 'unknown';
-  }
-};
-
-const getGnomeVersion = function () {
-  return Config.PACKAGE_VERSION;
-};
-
-const _repository = "http://github.com/OttoAllmendinger/" +
-                    "gnome-shell-bitcoin-markets";
-
-const _userAgent =  "gnome-shell-bitcoin-markets" +
-                    "/" + getExtensionVersion() +
-                    "/Gnome" + getGnomeVersion() +
-                    " (" + _repository + ")";
-
-
-// Some API providers have had issues with high traffic coming from single IPs
-// this code helps determine if these are actually different clients from behind
-// a NAT or if some clients really do many requests
-const getClientId = function () {
-  // GUID code from http://stackoverflow.com/a/2117523/92493
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-      var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
-      return v.toString(16);
-  });
-};
-
-const _clientId = getClientId();
-
-const _httpSession = new Soup.SessionAsync();
-
-
-_httpSession['user-agent'] = _userAgent;
-
-Soup.Session.prototype.add_feature.call(
-  _httpSession,
-  new Soup.ProxyResolverDefault()
-);
-
 
 const DefaultCurrencies = [
       'USD', 'EUR', 'CNY', 'GBP', 'CAD', 'RUB', 'AUD',
       'BRL', 'CZK', 'JPY', 'NZD', 'SEK', 'SGD', 'PLN',
       'MXN'
 ];
-
-const getJSON = function (url, callback) {
-  // log((new Date()) + ' getJSON ' + url);
-  let message = Soup.Message.new("GET", url);
-  let headers = message.request_headers;
-  headers.append('X-Client-Id', _clientId);
-  _httpSession.queue_message(
-    message,
-    (session, message) => {
-      if (message.status_code == 200) {
-        callback(null, JSON.parse(message.response_body.data));
-      } else {
-        log('getJSON error url: ' + url);
-        log('getJSON error status code: ' + message.status_code);
-        log('getJSON error response: ' + message.response_body.data);
-        callback(message.status_code, null);
-      }
-    }
-  );
-};
 
 const Selector = function (path) {
   /**
@@ -225,8 +152,8 @@ const Handler = new Lang.Class({
         this._lastData = data;
         this.emit("update", error, data);
 
-        if (Number(error) == HTTP_TOO_MANY_REQUESTS) {
-          log("error " + HTTP_TOO_MANY_REQUESTS + " - disable handler " + id);
+        if (HTTP.isErrTooManyRequests(error)) {
+          log("http error: too many requests: disable handler " + id);
           this.disabled = true;
         }
       }));
@@ -265,7 +192,7 @@ const Api = new Lang.Class({
 
   poll: function (options, callback) {
     let url = this.getUrl(options);
-    getJSON(url, callback);
+    HTTP.getJSON(url, callback);
   },
 
   _getHandlerId: function (options) {
