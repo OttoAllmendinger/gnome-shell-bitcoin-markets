7b24ef7d8a88d9d3c2443290c6b035bd26ab0d9a
Tudor Holton <tudor@tudorholton.com>
2018-01-08 12:16:32 +1100

    2 space indentation

--- src/ProviderBTCMarketsPrefs.js
+++ src/ProviderBTCMarketsPrefs.js
@@ -5,28 +5,24 @@ const Local = imports.misc.extensionUtils.getCurrentExtension();
 const { ProviderBTCMarkets } = Local.imports;
 const { BaseProviderConfigView } = Local.imports.BaseProviderConfigView;
 
-
 const ConfigView = new Lang.Class({
-	  Name: "ProviderBTCMarkets.ConfigView",
-	  Extends: BaseProviderConfigView,
-
-	  _init: function (configWidget, indicatorConfig) {
-		      this.parent(configWidget, indicatorConfig);
-		      this._addSelectCurrency((new ProviderBTCMarkets.Api()).currencies);
-		      this._addSelectCoin((new ProviderBTCMarkets.Api()).coins);
-		    },
-
-	  _setApiDefaults: function (config) {
-		      if (config.get('api') !== 'btcmarkets') {
-			            config.attributes = {
-					            api: 'btcmarkets',
-					            currency: 'USD',
-					            coin: 'BTC',
-					            attribute: 'last'
-					          };
-
-			            config.emit('update');
-			          }
-		    },
+  Name: "ProviderBTCMarkets.ConfigView",
+  Extends: BaseProviderConfigView,
+  _init: function (configWidget, indicatorConfig) {
+    this.parent(configWidget, indicatorConfig);
+    this._addSelectCurrency((new ProviderBTCMarkets.Api()).currencies);
+    this._addSelectCoin((new ProviderBTCMarkets.Api()).coins);
+  },
+  _setApiDefaults: function (config) {
+    if (config.get('api') !== 'btcmarkets') {
+      config.attributes = {
+        api: 'btcmarkets',
+        currency: 'USD',
+        coin: 'BTC',
+        attribute: 'last'
+      };
+      config.emit('update');
+    }
+  }
 });
 Signals.addSignalMethods(ConfigView.prototype);
