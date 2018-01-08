8c7871e6846a6ba867ab82bde869867956db08e0
Tudor Holton <tudor@tudorholton.com>
2018-01-08 12:16:32 +1100

    2 space indentation

--- src/ProviderBTCMarkets.js
+++ src/ProviderBTCMarkets.js
@@ -5,34 +5,26 @@ const BaseProvider = Local.imports.BaseProvider;
 
 
 const Api = new Lang.Class({
-	  Name: 'BTCMarkets.Api',
-	  Extends: BaseProvider.Api,
-
-	  apiName: "BTCMarkets",
-
-	  currencies: ['AUD', 'BTC'],
-
-	  coins: ['BTC', 'LTC', 'ETH', 'ETC', 'XRP', 'BCH'],
-
-	  interval: 10,
-
-	  attributes: {
-		      last: function (options) {
-			            const renderCurrency = BaseProvider.CurrencyRenderer(options);
-			            const renderChange = BaseProvider.ChangeRenderer();
-
-			            return {
-					            text: (data) => renderCurrency(data.lastPrice),
-					            change: (data) => renderChange(data.lastPrice)
-					          };
-			          }
-		    },
-
-	  getLabel: function (options) {
-		      return 'BTCMarkets.net ' + options.currency + '/' + options.coin;
-		    },
-
-	  getUrl: function (options) {
-		      return 'https://api.btcmarkets.net/market/' + options.coin + '/' + options.currency + '/tick';
-		    }
+  Name: 'BTCMarkets.Api',
+  Extends: BaseProvider.Api,
+  apiName: "BTCMarkets",
+  currencies: ['AUD', 'BTC'],
+  coins: ['BTC', 'LTC', 'ETH', 'ETC', 'XRP', 'BCH'],
+  interval: 10,
+  attributes: {
+    last: function (options) {
+      const renderCurrency = BaseProvider.CurrencyRenderer(options);
+      const renderChange = BaseProvider.ChangeRenderer();
+      return {
+        text: (data) => renderCurrency(data.lastPrice),
+        change: (data) => renderChange(data.lastPrice)
+      };
+    }
+  },
+  getLabel: function (options) {
+    return 'BTCMarkets.net ' + options.currency + '/' + options.coin;
+  },
+  getUrl: function (options) {
+    return 'https://api.btcmarkets.net/market/' + options.coin + '/' + options.currency + '/tick';
+  }
 });
