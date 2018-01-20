3b8d3bc55df4d8ab87b16cbf57e94417e8fe6124
Martin Szulecki <martin.szulecki@gmail.com>
2018-01-20 02:24:53 +0100

    Add support for all trading pairs to Kraken provider

--- src/ProviderKraken.js
+++ src/ProviderKraken.js
@@ -2,23 +2,59 @@ const Lang = imports.lang;
 
 const Local = imports.misc.extensionUtils.getCurrentExtension();
 const BaseProvider = Local.imports.BaseProvider;
+const ProviderKraken = Local.imports.ProviderKraken;
+const AssetPairs = Local.imports.ProviderKrakenAssetPairs.AssetPairs;
 
+var getPairByName = (name) => {
+  return AssetPairs[name];
+};
 
-// horrific api
-// curl https://api.kraken.com/0/public/AssetPairs | jq '.[] | keys'
-const getPairCode = (coin, currency) => {
-  coin = coin.toUpperCase()
-  currency = currency.toUpperCase()
-  if (coin.toUpperCase() === 'BTC') {
-    coin = 'XBT';
-  }
-  const unprefixed = ['BCH', 'DASH', 'EOS', 'GNO'];
-  if (unprefixed.includes(coin)) {
-    return `${coin}${currency}`;
-  } else {
-    return `X${coin}Z${currency}`;
-  }
+var getPairNameByCurrencyAndCoin = (coin, currency) => {
+  let found = Object.keys(AssetPairs).filter((x, i, a) => ((AssetPairs[x].base == coin) && (AssetPairs[x].quote == currency)));
+  if (found.length > 0)
+    return found[0];
+  else
+    return undefined;
 }
+
+var getCurrenciesForCoin = (coin) => {
+  let currencies = [];
+  let found = Object.keys(AssetPairs).filter((x, i, a) => (AssetPairs[x].base == coin));
+  found.forEach((pair_name) => {
+    let pair = AssetPairs[pair_name];
+    if (currencies.indexOf(pair.quote) == -1) {
+      currencies.push(pair.quote);
+    }
+  });
+  return currencies;
+}
+
+var getCoinsForCurrency = (currency) => {
+  let coins = [];
+  let found = Object.keys(AssetPairs).filter((x, i, a) => (AssetPairs[x].quote == currency));
+  found.forEach((pair_name) => {
+    let pair = AssetPairs[pair_name];
+    if (coins.indexOf(pair.currency) == -1) {
+      coins.push(pair.currency);
+    }
+  });
+  return coins;
+}
+
+var getCurrencyFromKrakenQuote = (symbol) => {
+  return symbol.substr(1);
+}
+
+var getCoinFromKrakenBase = (symbol) => {
+  if (symbol.charAt(0) == 'X') {
+    return symbol.substr(1);
+  }
+  return symbol;
+};
+
+const calculateCurrentPrice = (price_data) => {
+  return ((parseFloat(price_data["a"][0]) + parseFloat(price_data["b"][0])) / 2);
+};
 
 var Api = new Lang.Class({
   Name: 'Kraken.Api',
@@ -26,42 +62,66 @@ var Api = new Lang.Class({
 
   apiName: "Kraken",
 
-  currencies: ['USD', 'EUR'],
+  currencies: [],
 
-  coins: [
-    'BTC',
-    'BCH',
-    'DASH',
-    'ETH',
-    'LTC',
-    'ZEC',
-    'XMR',
-    'REP',
-    'XRP',
-    'ETC'
-  ],
+  coins: [],
 
-  interval: 10, // unknown, guessing
+  // See https://www.kraken.com/help/api#api-call-rate-limit
+  interval: 10,
+
+  _init: function () {
+    this.parent();
+    this._updateExchangeData();
+  },
+
+  _updateExchangeData: function() {
+    let pairs = AssetPairs;
+    Object.keys(pairs).forEach((key) => {
+      let pair = pairs[key];
+      if (this.currencies.indexOf(pair.quote) == -1) {
+        this.currencies.push(pair.quote);
+      }
+      if (this.coins.indexOf(pair.base) == -1) {
+        this.coins.push(pair.base);
+      }
+    });
+  },
 
   attributes: {
-    last: function(options) {
-      const renderCurrency = BaseProvider.CurrencyRenderer(options);
+    last: function (options) {
+      let pair = getPairByName(options.pair);
+
+      let decimals = (options.decimals === undefined ? pair.pair_decimals: options.decimals);
+
+      let info = {
+        currency: getCurrencyFromKrakenQuote(pair.quote),
+        coin: getCoinFromKrakenBase(pair.base),
+        decimals: decimals
+      };
+      const renderCurrency = BaseProvider.CurrencyRenderer(info);
       const renderChange = BaseProvider.ChangeRenderer();
-      const { coin, currency } = options;
-      const id = getPairCode(coin, currency);
+
+      let pair_name = options.pair;
+
       return {
-        text: ({result}) => renderCurrency(result[id].a[0]),
-        change: ({result}) => renderChange(result[id].a[0])
+        text: (data) => renderCurrency(calculateCurrentPrice(data["result"][pair_name])),
+        change: (data) => renderChange(calculateCurrentPrice(data["result"][pair_name]))
       };
     }
   },
 
-  getLabel: function({coin, currency}) {
-    return "Kraken " + currency + "/" + coin;
+  getLabel: function(options) {
+    let pair = getPairByName(options.pair);
+    return "Kraken " + getCurrencyFromKrakenQuote(pair.quote) + "/" + getCoinFromKrakenBase(pair.base);
   },
 
-  getUrl: function({coin, currency}) {
-    return "https://api.kraken.com/0/public/Ticker?pair=" +
-      getPairCode(coin, currency);
+  getUrl: function(options) {
+    let pair = getPairByName(options.pair);
+    if (pair === undefined) {
+      return;
+    }
+
+    // See https://www.kraken.com/help/api#get-ticker-info
+    return "https://api.kraken.com/0/public/Ticker?pair=" + options.pair;
   }
 });
--- src/ProviderKrakenPrefs.js
+++ src/ProviderKrakenPrefs.js
@@ -12,16 +12,82 @@ var ConfigView = new Lang.Class({
 
   _init: function (configWidget, indicatorConfig) {
     this.parent(configWidget, indicatorConfig);
-    this._addSelectCurrency((new ProviderKraken.Api()).currencies);
-    this._addSelectCoin((new ProviderKraken.Api()).coins);
+
+    let api = new ProviderKraken.Api();
+
+    let currencyWidgets = this._addSelectCurrency(api.currencies);
+    let coinWidgets = this._addSelectCoin(api.coins);
+
+    // make sure to only enable available currencies for this coin on init
+    let selected_currency = this._indicatorConfig.get('currency');
+    let allowed_currencies = ProviderKraken.getCurrenciesForCoin(this._indicatorConfig.get('coin'));
+    this._updateComboBoxSensitiveItems(currencyWidgets.comboBoxView, allowed_currencies, selected_currency);
+
+    currencyWidgets.comboBoxView.connect('changed', (view, value) => {
+      let coinComboBoxView = coinWidgets.comboBoxView;
+
+      // get selected coin
+      let coin = this._getComboBoxActiveItem(coinComboBoxView);
+      let currency = value;
+
+      let pair_name = ProviderKraken.getPairNameByCurrencyAndCoin(coin, currency);
+      this._indicatorConfig.set('pair', pair_name);
+    });
+
+    coinWidgets.comboBoxView.connect('changed', (view, value) => {
+      let currencyComboBoxView = currencyWidgets.comboBoxView;
+
+      // get selected currency
+      let currency = this._getComboBoxActiveItem(currencyComboBoxView);
+      let coin = value;
+
+      // only enable currencies available for this coin
+      let allowed_currencies = ProviderKraken.getCurrenciesForCoin(coin);
+      currency = this._updateComboBoxSensitiveItems(currencyComboBoxView, allowed_currencies, currency);
+
+      let pair_name = ProviderKraken.getPairNameByCurrencyAndCoin(coin, currency);
+      this._indicatorConfig.set('pair', pair_name);
+    });
+  },
+
+  _getComboBoxActiveItem: function (view) {
+    let active = view.widget.get_active();
+    return view._options[active].value;
+  },
+
+  _updateComboBoxSensitiveItems: function (view, allowed_values, selected_value) {
+      // update sensitivivity state for each item
+      view.model.foreach((model, path, iter) => {
+        let label = model.get_value(iter, view.Columns.LABEL);
+        if (allowed_values.indexOf(label) != -1) {
+          model.set_value(iter, view.Columns.SENSITIVE, true);
+        } else {
+          model.set_value(iter, view.Columns.SENSITIVE, false);
+        }
+      });
+
+      // select next allowed currency if needed
+      if (allowed_values.indexOf(selected_value) == -1) {
+        view.model.foreach((model, path, iter) => {
+          let is_sensitive = model.get_value(iter, view.Columns.SENSITIVE);
+          if (is_sensitive == true) {
+            selected_value = model.get_value(iter, view.Columns.LABEL);
+            view.widget.set_active_iter(iter);
+            return selected_value;
+          }
+        });
+      }
+
+      return selected_value;
   },
 
   _setApiDefaults: function (config) {
     if (config.get('api') !== 'kraken') {
       config.attributes = {
         api: 'kraken',
-        currency: 'EUR',
-        coin: 'XBT',
+        pair: 'XXBTZEUR',
+        currency: 'ZEUR',
+        coin: 'XXBT',
         attribute: 'last'
       };
 
