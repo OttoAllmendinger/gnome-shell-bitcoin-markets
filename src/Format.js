ff54e7f11057a282fe686ce141c200047b047531
Otto Allmendinger <otto@bitgo.com>
2019-01-12 19:34:19 +0100

    Add prefix `sat` (1e8) for satoshi-quoted coins
    This is useful for coins with very small BTC value.
    
    Fixes #77

--- src/Format.js
+++ src/Format.js
@@ -37,7 +37,7 @@ const format = (value, { base, quote, format }) => {
     );
   }
 
-  const scale = [["", 1], ["m", 0.001], ["k", 1000]];
+  const scale = [["", 1], ["m", 0.001], ["k", 1000], ["sat", 1e8]];
   scale.forEach(([prefix, scale]) => {
     formatData[`${prefix}v`] = formatValueWithDigits(
       value, scale, info ? info.decimal_digits : defaultDigits
@@ -64,13 +64,14 @@ const tooltipText = () => {
     ["bs",       _("Base currency symbol")],
     ["btc",      _("Bitcoin symbol (₿)")],
     ["v",        _("formatted value with defaults")],
-    ["mv",       _("formatted value with defaults, divided by 1000")],
-    ["kv",       _("formatted value with defaults, multiplied by 1000")],
-    ["(m|k)v0",  _("formatted value with 0 decimals")],
-    ["(m|k)v1",  _("formatted value with 1 decimal")],
-    ["(m|k)v2",  _("formatted value with 2 decimals")],
+    ["mv",       _("formatted value with defaults, divided by ") + (0.001).toLocaleString()],
+    ["kv",       _("formatted value with defaults, multiplied by ") + (1000).toLocaleString()],
+    ["satv",     _("formatted value with defaults, multiplied by ") + (1e8).toLocaleString()],
+    ["(m|k|sat)v0",  _("formatted value with 0 decimals")],
+    ["(m|k|sat)v1",  _("formatted value with 1 decimal")],
+    ["(m|k|sat)v2",  _("formatted value with 2 decimals")],
     ["...", ""],
-    ["(m|k)v8",  _("formatted value with 8 decimals")],
+    ["(m|k|sat)v8",  _("formatted value with 8 decimals")],
     ["raw",      _("raw value without additional formatting")]
   ].map(([a, b]) => `<tt>${pad(a, 16)}</tt>${b}`).join("\n")
 }
