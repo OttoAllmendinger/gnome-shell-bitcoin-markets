7765edcf8d006124bd33c669e95b915050253bdb
Ross Golder <ross@golder.org>
2019-09-21 15:30:27 +0700

    Add Satang.pro support.

---
+++ src/ProviderSatangPro.js
@@ -0,0 +1,28 @@
+const Lang = imports.lang;
+
+const Local = imports.misc.extensionUtils.getCurrentExtension();
+const BaseProvider = Local.imports.BaseProvider;
+
+
+const Api = new Lang.Class({
+  Name: "SatangPro.Api",
+  Extends: BaseProvider.Api,
+
+  apiName: "Satang.pro",
+
+  apiDocs: [
+    ["API Docs", "https://docs.satang.pro/apis/public/orders"]
+  ],
+
+  interval: 60, // unclear, should be safe
+
+  getUrl({ base, quote }) {
+    return `https://api.tdax.com/api/orders/?pair=${base}_${quote}`;
+  },
+
+  getLast(data) {
+    var bidding = parseFloat(data['bid'][0]['price']);
+    var asking = parseFloat(data['ask'][0]['price']);
+    return ((asking - bidding) * 0.5) + bidding;
+  }
+});
