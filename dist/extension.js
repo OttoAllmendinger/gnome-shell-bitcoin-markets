30199b6ff8cf5da86cb784cea0b52d3f6c06c3a2
Otto Allmendinger <mail@ttll.de>
2024-12-28 13:42:32 +0100

    Update build artifacts

--- dist/extension.js
+++ dist/extension.js
@@ -2,11 +2,11 @@ import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 import GObject from 'gi://GObject';
-import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
-import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import { panel } from 'resource:///org/gnome/shell/ui/main.js';
+import { Button } from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import { PopupMenuItem, PopupSeparatorMenuItem } from 'resource:///org/gnome/shell/ui/popupMenu.js';
 import GLib from 'gi://GLib';
-import * as Config from 'resource:///org/gnome/shell/misc/config.js';
+import { PACKAGE_VERSION } from 'resource:///org/gnome/shell/misc/config.js';
 import Soup from 'gi://Soup';
 import Gio from 'gi://Gio';
 
@@ -32,7 +32,7 @@ function __decorate(decorators, target, key, desc) {
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 }
 
-typeof SuppressedError === "function" ? SuppressedError : function (error, suppressed, message) {
+var _SuppressedError = typeof SuppressedError === "function" ? SuppressedError : function (error, suppressed, message) {
     var e = new Error(message);
     return e.name = "SuppressedError", e.error = error, e.suppressed = suppressed, e;
 };
@@ -192,7 +192,7 @@ async function getJSON(url, { userAgent }) {
 /**
  * Api definitions
  */
-class Api$t {
+class Api {
     tickers = [];
     getLabel({ base, quote }) {
         return `${this.apiName} ${base}/${quote}`;
@@ -223,7 +223,7 @@ class Api$t {
     }
 }
 
-class Api$s extends Api$t {
+class Api$1 extends Api {
     apiName = 'Binance';
     apiDocs = [['API Docs', 'https://binance-docs.github.io/apidocs/spot/en/#symbol-price-ticker']];
     interval = 15;
@@ -238,7 +238,7 @@ class Api$s extends Api$t {
     }
 }
 
-class Api$r extends Api$t {
+class Api$2 extends Api {
     apiName = 'Binance Futures';
     apiDocs = [
         [
@@ -258,7 +258,7 @@ class Api$r extends Api$t {
     }
 }
 
-class Api$q extends Api$t {
+class Api$3 extends Api {
     apiName = 'Bit2C';
     apiDocs = [['API Docs', 'https://bit2c.co.il/home/api']];
     interval = 10; // unknown, guessing
@@ -276,7 +276,7 @@ class Api$q extends Api$t {
     }
 }
 
-class Api$p extends Api$t {
+class Api$4 extends Api {
     apiName = 'Bitfinex';
     apiDocs = [
         ['API Docs', 'https://docs.bitfinex.com/v1/reference#rest-public-ticker'],
@@ -316,7 +316,7 @@ class Api$p extends Api$t {
     }
 }
 
-class Api$o extends Api$t {
+class Api$5 extends Api {
     apiName = 'BitMEX';
     apiDocs = [['API Docs', 'https://www.bitmex.com/app/restAPI']];
     // ```
@@ -342,7 +342,7 @@ class Api$o extends Api$t {
     }
 }
 
-class Api$n extends Api$t {
+class Api$6 extends Api {
     apiName = 'BitPay';
     apiDocs = [['API Docs', 'https://bitpay.com/api']];
     interval = 60; // unclear, should be safe
@@ -358,7 +358,7 @@ class Api$n extends Api$t {
     }
 }
 
-class Api$m extends Api$t {
+class Api$7 extends Api {
     apiName = 'Bitso';
     apiDocs = [
         ['API Docs', 'https://bitso.com/api_info#http-api-responses'],
@@ -378,7 +378,7 @@ class Api$m extends Api$t {
     }
 }
 
-class Api$l extends Api$t {
+class Api$8 extends Api {
     apiName = 'Bitstamp';
     apiDocs = [['API Docs', 'https://www.bitstamp.net/api/']];
     // Quote 2013-08-09  ---  https://www.bitstamp.net/api/
@@ -393,7 +393,7 @@ class Api$l extends Api$t {
     }
 }
 
-class Api$k extends Api$t {
+class Api$9 extends Api {
     apiName = 'Bittrex';
     apiDocs = [['API Docs', 'https://bittrex.github.io/api/v3#operation--markets--marketSymbol--ticker-get']];
     interval = 15;
@@ -405,7 +405,7 @@ class Api$k extends Api$t {
     }
 }
 
-class Api$j extends Api$t {
+class Api$a extends Api {
     apiName = 'Buda';
     apiDocs = [['API Docs', 'https://api.buda.com/#la-api-de-buda-com']];
     interval = 60;
@@ -420,7 +420,7 @@ class Api$j extends Api$t {
     }
 }
 
-class Api$i extends Api$t {
+class Api$b extends Api {
     apiName = 'BTCMarkets';
     apiDocs = [
         ['API Docs', 'https://github.com/BTCMarkets/API/wiki/Market-data-API'],
@@ -442,7 +442,7 @@ class Api$i extends Api$t {
     }
 }
 
-class Api$h extends Api$t {
+class Api$c extends Api {
     apiName = 'CEX.IO';
     apiDocs = [
         ['API Docs', 'https://cex.io/rest-api#ticker'],
@@ -460,7 +460,7 @@ class Api$h extends Api$t {
     }
 }
 
-class Api$g extends Api$t {
+class Api$d extends Api {
     apiName = 'Coinbase';
     apiDocs = [['API Docs', 'https://developers.coinbase.com/api/v2#exchange-rates']];
     interval = 60; // unclear, should be safe
@@ -481,7 +481,7 @@ class Api$g extends Api$t {
     }
 }
 
-class Api$f extends Api$t {
+class Api$e extends Api {
     apiName = 'CoinGecko';
     apiDocs = [
         ['API Docs', 'https://www.coingecko.com/api/docs/v3#/coins/get_coins_list'],
@@ -511,7 +511,7 @@ class Api$f extends Api$t {
     }
 }
 
-class Api$e extends Api$t {
+class Api$f extends Api {
     apiName = 'CryptoCompare';
     apiDocs = [['API Docs', 'https://min-api.cryptocompare.com/documentation']];
     interval = 15;
@@ -526,7 +526,7 @@ class Api$e extends Api$t {
     }
 }
 
-class Api$d extends Api$t {
+class Api$g extends Api {
     apiName = 'FTX exchange';
     apiDocs = [['API Docs', 'https://docs.ftx.com/#get-markets']];
     interval = 15;
@@ -538,7 +538,7 @@ class Api$d extends Api$t {
     }
 }
 
-class Api$c extends Api$t {
+class Api$h extends Api {
     apiName = 'Gate.io';
     apiDocs = [['API Docs', 'https://www.gate.io/docs/developers/apiv4']];
     interval = 60; // unknown, guessing
@@ -556,7 +556,7 @@ class Api$c extends Api$t {
     }
 }
 
-class Api$b extends Api$t {
+class Api$i extends Api {
     apiName = 'HitBTC';
     apiDocs = [['API Docs', 'https://api.hitbtc.com/']];
     interval = 15;
@@ -568,7 +568,7 @@ class Api$b extends Api$t {
     }
 }
 
-class Api$a extends Api$t {
+class Api$j extends Api {
     apiName = 'Huobi';
     apiDocs = [['API Docs', 'https://huobiapi.github.io/docs/spot/v1/en/#introduction']];
     // Each API Key can send maximum of 100 https requests within 10 seconds
@@ -588,7 +588,7 @@ class Api$a extends Api$t {
     }
 }
 
-class Api$9 extends Api$t {
+class Api$k extends Api {
     apiName = 'Kraken';
     apiDocs = [
         ['API Docs', 'https://www.kraken.com/help/api#public-market-data'],
@@ -613,7 +613,7 @@ class Api$9 extends Api$t {
     }
 }
 
-class Api$8 extends Api$t {
+class Api$l extends Api {
     apiName = 'Kucoin';
     apiDocs = [['API Docs', 'https://docs.kucoin.com/']];
     interval = 15;
@@ -631,7 +631,7 @@ class Api$8 extends Api$t {
     }
 }
 
-class Api$7 extends Api$t {
+class Api$m extends Api {
     apiName = 'MEXC';
     apiDocs = [['API Docs', 'https://mexcdevelop.github.io/apidocs/spot_v3_en']];
     interval = 10; // unknown, guessing
@@ -647,7 +647,7 @@ class Api$7 extends Api$t {
     }
 }
 
-class Api$6 extends Api$t {
+class Api$n extends Api {
     apiName = 'Nobitex';
     apiDocs = [['API Docs', 'https://apidocs.nobitex.ir/#quickstart']];
     interval = 15;
@@ -662,7 +662,7 @@ class Api$6 extends Api$t {
     }
 }
 
-class Api$5 extends Api$t {
+class Api$o extends Api {
     apiName = 'Paymium';
     apiDocs = [['API Docs', 'https://github.com/Paymium/api-documentation#ticker']];
     interval = 60; // unclear, should be safe
@@ -684,7 +684,7 @@ class Api$5 extends Api$t {
     }
 }
 
-class Api$4 extends Api$t {
+class Api$p extends Api {
     apiName = 'Satang.pro';
     apiDocs = [['API Docs', 'https://docs.satang.pro/apis/public/orders']];
     interval = 60; // unclear, should be safe
@@ -720,7 +720,7 @@ function getTokenInfo(code) {
     }
     return tokenInfo[code];
 }
-class Api$3 extends Api$t {
+class Api$q extends Api {
     apiName = 'TomoX(TomoChain)';
     apiDocs = [['API Docs', 'https://apidocs.tomochain.com/#tomodex-apis-trades']];
     interval = 15;
@@ -743,7 +743,7 @@ class Api$3 extends Api$t {
     }
 }
 
-class Api$2 extends Api$t {
+class Api$r extends Api {
     apiName = 'VccExchange(Vietnam)';
     apiDocs = [['API Docs', 'https://vcc.exchange/api']];
     interval = 15;
@@ -761,7 +761,7 @@ class Api$2 extends Api$t {
     }
 }
 
-class Api$1 extends Api$t {
+class Api$s extends Api {
     apiName = 'Bybit';
     apiDocs = [
         ['API Docs', 'https://bybit-exchange.github.io/docs/v5/market/tickers'],
@@ -787,7 +787,7 @@ class Api$1 extends Api$t {
     }
 }
 
-class Api extends Api$t {
+class Api$t extends Api {
     apiName = 'Bybit Perpetual';
     apiDocs = [
         ['API Docs', 'https://bybit-exchange.github.io/docs/v5/market/tickers'],
@@ -814,36 +814,36 @@ class Api extends Api$t {
 }
 
 const Providers = {
-    binance: new Api$s(),
-    binanceFutures: new Api$r(),
-    bit2c: new Api$q(),
-    bitfinex: new Api$p(),
-    bitmex: new Api$o(),
-    bitpay: new Api$n(),
-    bitso: new Api$m(),
-    bitstamp: new Api$l(),
-    bittrex: new Api$k(),
-    btcmarkets: new Api$i(),
-    buda: new Api$j(),
-    bybit: new Api$1(),
-    bybitPerpetual: new Api(),
-    cexio: new Api$h(),
-    coinbase: new Api$g(),
-    coingecko: new Api$f(),
-    cryptocompare: new Api$e(),
-    ftx: new Api$d(),
-    gate: new Api$c(),
-    hitbtc: new Api$b(),
-    huobi: new Api$a(),
-    kraken: new Api$9(),
-    kucoin: new Api$8(),
-    mexc: new Api$7(),
-    nobitex: new Api$6(),
-    paymium: new Api$5(),
-    poloniex: new Api$n(),
-    satangpro: new Api$4(),
-    tomox: new Api$3(),
-    vccexchange: new Api$2(),
+    binance: new Api$1(),
+    binanceFutures: new Api$2(),
+    bit2c: new Api$3(),
+    bitfinex: new Api$4(),
+    bitmex: new Api$5(),
+    bitpay: new Api$6(),
+    bitso: new Api$7(),
+    bitstamp: new Api$8(),
+    bittrex: new Api$9(),
+    btcmarkets: new Api$b(),
+    buda: new Api$a(),
+    bybit: new Api$s(),
+    bybitPerpetual: new Api$t(),
+    cexio: new Api$c(),
+    coinbase: new Api$d(),
+    coingecko: new Api$e(),
+    cryptocompare: new Api$f(),
+    ftx: new Api$g(),
+    gate: new Api$h(),
+    hitbtc: new Api$i(),
+    huobi: new Api$j(),
+    kraken: new Api$k(),
+    kucoin: new Api$l(),
+    mexc: new Api$m(),
+    nobitex: new Api$n(),
+    paymium: new Api$o(),
+    poloniex: new Api$6(),
+    satangpro: new Api$p(),
+    tomox: new Api$q(),
+    vccexchange: new Api$r(),
 };
 function getProvider(name) {
     if (name in Providers) {
@@ -994,7 +994,7 @@ class PollLoop {
         }
         try {
             const response = await getJSON(url, {
-                userAgent: getDefaultUserAgent(ext.metadata, Config.PACKAGE_VERSION),
+                userAgent: getDefaultUserAgent(ext.metadata, PACKAGE_VERSION),
             });
             const date = new Date();
             this.cache.set(url, { date, response });
@@ -2118,7 +2118,7 @@ const _Symbols = {
     down: '\u25bc',
     unchanged: ' ',
 };
-let MarketIndicatorView = class MarketIndicatorView extends PanelMenu.Button {
+let MarketIndicatorView = class MarketIndicatorView extends Button {
     ext;
     options;
     providerLabel;
@@ -2159,7 +2159,7 @@ let MarketIndicatorView = class MarketIndicatorView extends PanelMenu.Button {
         layout.add_child(this._statusView);
         layout.add_child(this._indicatorView);
         this.add_child(layout);
-        this._popupItemStatus = new PopupMenu.PopupMenuItem('', {
+        this._popupItemStatus = new PopupMenuItem('', {
             activate: false,
             hover: false,
             can_focus: false,
@@ -2167,8 +2167,8 @@ let MarketIndicatorView = class MarketIndicatorView extends PanelMenu.Button {
         this._popupItemStatus.label.set_style('max-width: 12em;');
         this._popupItemStatus.label.clutter_text.set_line_wrap(true);
         this.menu.addMenuItem(this._popupItemStatus);
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-        this._popupItemSettings = new PopupMenu.PopupMenuItem('Settings');
+        this.menu.addMenuItem(new PopupSeparatorMenuItem());
+        this._popupItemSettings = new PopupMenuItem('Settings');
         this.menu.addMenuItem(this._popupItemSettings);
         this._popupItemSettings.connect('activate', () => {
             this.ext.openPreferences();
@@ -2326,7 +2326,7 @@ class IndicatorCollection {
                 return new MarketIndicatorView(this.ext, options);
             });
             indicators.forEach((view, i) => {
-                Main.panel.addToStatusArea(`bitcoin-market-indicator-${i}`, view);
+                panel.addToStatusArea(`bitcoin-market-indicator-${i}`, view);
             });
             this._indicators = indicators;
         }
@@ -2362,4 +2362,4 @@ class BitcoinMarketsExtension extends Extension {
     }
 }
 
-export { BitcoinMarketsExtension as default };
+export default BitcoinMarketsExtension;
